a0716108d58203cc340cf34422934d4125f9f2aa
Diego <diego2000avelar@gmail.com>
2021-03-24 19:29:30 -0600

    mixins

--- app/bases/views.py
+++ app/bases/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # create view
-class Home(generic.TemplateView):
+class Home(LoginRequiredMixin, generic.TemplateView):
+    login_url = '/admin/'
     template_name = 'bases/home.html'
 
